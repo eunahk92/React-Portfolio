cb8d46fd7a18185b0b38a7fec98e7d69c4a0348a
Eunah Kim <Eunahkim92@gmail.com>
2020-08-12 17:38:31 -0400

    Add footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <nav className="navbar fixed-bottom navbar-light bg-light">
+        <span className="navbar-brand">
+            <a className="ml-4 text-dark" href="https://github.com/eunahk92">
+                <i className="fab fa-github"></i>
+            </a>
+            <a className="ml-4 text-dark" href="https://linkedin.com/eunahjkim">
+                <i className="fab fa-linkedin"></i>
+            </a>
+            <a className="ml-4 text-dark" href="mailto:eunahkim92@gmail.com">
+                <i className="fas fa-envelope"></i>
+            </a>
+        </span>
+
+    </nav>
+  );
+}
+
+export default Footer;
