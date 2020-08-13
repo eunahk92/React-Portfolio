fb937ceeb29d03238a27660392a2223d243f04f5
Eunah Kim <Eunahkim92@gmail.com>
2020-08-12 23:23:32 -0400

    Refactor component to remove hardcoded message

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import './style.css';
 
-const Jumbotron = () => {
-   return (
-        <div className="mt-5">
-            <h1 className="bigWord m-0">Hello,</h1> 
-            <h1 className="bigWord m-0">my name is <span className="bigWordName m-0">Eunah Kim</span></h1>
-            <h4 className="text-right">and welcome to my page.</h4><br />
-        </div>
-   ) 
-};
+const Jumbotron = ({ children }) => <div className="mt-5">{children}</div>;
 
 export default Jumbotron;
 
