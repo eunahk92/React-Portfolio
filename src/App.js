df535dbaee35c3825a8ed21933eefe75a4a15144
Eunah Kim <Eunahkim92@gmail.com>
2020-08-12 23:15:58 -0400

    Feat new Route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Wrapper from './components/Wrapper';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from './pages/Home'
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
 
 const App = () => {
     return (
-    <Router>
-        <div>
-            <Navbar />
-            {/* <Wrapper>
-                <Route exact path="/" />
-                <Route exact path="/about" />
-                <Route exact path="/portfolio" />
-                <Route exact path="/contactMe" />
-            </Wrapper> */}
-        </div>
-    </Router>
+        <Router>
+            <div>
+                <Nav />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/portfolio" component={Portfolio} />
+                </Switch>
+                <Footer />
+            </div>
+        </Router>
     )
 };
 
