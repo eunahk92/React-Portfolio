97f04792dd71e9e36308a71e7292519dfa9e2a9b
Eunah Kim <Eunahkim92@gmail.com>
2020-08-15 16:15:41 -0400

    Refactor routes and pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Nav from './components/Nav';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import About from './pages/About';
-import Portfolio from './pages/Portfolio';
 import Wrapper from './components/Wrapper';
+import About from './pages/About';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
 
 const App = () => {
     return (
         <Router>
             <div>
-                <Nav />
+                <Navbar />
                 <Wrapper>
                     <Switch>
-                        <Route exact path="/" component={About} />
+                        <Route exact path="/" component={Home} />
                         <Route exact path="/portfolio" component={Portfolio} />
-                        <Route exact path="*" component={About} />
+                        <Route exact path="/aboutme" component={About} />
                     </Switch>
                 </Wrapper>
                 <Footer />
