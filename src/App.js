548bde931257ff0742617bf6b68f72729e45aef6
Eunah Kim <Eunahkim92@gmail.com>
2020-08-14 23:45:32 -0400

    Remove about route and combine with root route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import Home from './pages/Home'
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Wrapper from './components/Wrapper';
@@ -14,10 +13,9 @@ const App = () => {
                 <Nav />
                 <Wrapper>
                     <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/about" component={About} />
+                        <Route exact path="/" component={About} />
                         <Route exact path="/portfolio" component={Portfolio} />
-                        <Route exact path="*" component={Home} />
+                        <Route exact path="*" component={About} />
                     </Switch>
                 </Wrapper>
                 <Footer />
