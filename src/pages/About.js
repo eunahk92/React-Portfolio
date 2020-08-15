f359270c2ec610c0110739725b7f1e772a208341
Eunah Kim <Eunahkim92@gmail.com>
2020-08-14 23:47:16 -0400

    Combine home route with about route

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container } from '../components/Grid';
+import { Container, Row, Col } from '../components/Grid';
 import Jumbotron from '../components/Jumbotron';
+import picture from '../assets/profile_picture.png';
 
 const Home = () => {
    return (
@@ -10,6 +11,14 @@ const Home = () => {
             <h1 className="bigWord m-0">my name is <span className="bigWordName m-0">Eunah Kim</span></h1>
             <h4 className="text-right">and welcome to my page.</h4><br />
          </Jumbotron>
+         <Row>
+            <Col size="4">   
+               <img src={picture} className="img-thumbnail" alt="eunah-picture" />
+            </Col>
+            <Col size="8">   
+               <h4>Hello world.</h4>
+            </Col>
+         </Row>
       </Container>
    ) 
 };
