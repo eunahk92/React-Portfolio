10e5a84f39b880464323394e54cd003e519d5ab0
Eunah Kim <Eunahkim92@gmail.com>
2020-08-15 16:16:07 -0400

    Fix footer with correct links and styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React from 'react';
+import Resume from '../../assets/Resume.pdf';
 
 const Footer = () => {
-  return (
-    <nav className="navbar fixed-bottom navbar-light bg-light" id="footer">
-        <span className="navbar-brand">
-            <a className="ml-4 text-dark" href="https://github.com/eunahk92">
-                <i className="fab fa-github"></i>
-            </a>
-            <a className="ml-4 text-dark" href="https://linkedin.com/eunahjkim">
-                <i className="fab fa-linkedin"></i>
-            </a>
-            <a className="ml-4 text-dark" href="mailto:eunahkim92@gmail.com">
-                <i className="fas fa-envelope"></i>
-            </a>
-        </span>
-    </nav>
-  );
-}
+	return (
+        <nav className="navbar fixed-bottom navbar-light bg-light" id="footer">
+            <span className="navbar-brand mx-auto">
+                <a 
+                    className="ml-4 text-dark" 
+                    href="https://github.com/eunahk92" 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    title="Github Repositories"
+                >
+                    <i className="fab fa-github fa-2x"></i>
+                </a>
+                <a 
+                    className="ml-4 text-dark" 
+                    href="https://www.linkedin.com/in/eunahjkim/" 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    title="Linkedin"
+                >
+                    <i className="fab fa-linkedin fa-2x"></i>
+                </a>
+                <a 
+                    className="ml-4 text-dark" 
+                    href="mailto:eunahkim92@gmail.com" 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    title="Email Me"
+                >
+                    <i className="fas fa-envelope fa-2x"></i>
+                </a>
+                <a 
+                    className="ml-4 text-dark" 
+                    title="Download Resume" 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    href={Resume} download
+                >
+                    <i className="fas fa-file-download fa-2x"></i>
+                </a>
+            </span>
+        </nav>
+	);
+};
 
 export default Footer;
